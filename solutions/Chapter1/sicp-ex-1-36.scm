2bfd50727c2d8a506da1814a11d34886f0db0f1b
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-01-26 17:35:09 +0800

    Update sicp-ex-1-36.scm

--- solutions/Chapter1/sicp-ex-1-36.scm
+++ solutions/Chapter1/sicp-ex-1-36.scm
@@ -19,11 +19,6 @@
           (try next (inc cnt)))))
   (try first-guess 1))
 
-; let x=1+1/x
-; we get x^2-x-1=0
-; x=\frac{1}{2}(1\pm\sqrt{5})
-; \phi=\frac{1}{2}(1+\sqrt{5}) is one of the fixed-points of x=>1+1/x
-
 (fixed-point (lambda (x) (/ (log 1000) (log x))) 2.0)
 ; step34: 4.555540912917957
 (fixed-point (lambda (x) (average x (/ (log 1000) (log x)))) 2.0)

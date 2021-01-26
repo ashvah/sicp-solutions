514da32f0b0e89663295d94b9e6a3167ff15ecd8
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-01-26 17:26:05 +0800

    Create sicp-ex-1-35.scm

---
+++ solutions/Chapter1/sicp-ex-1-35.scm
@@ -0,0 +1,23 @@
+#lang sicp
+
+(define (average x y) (/ (+ x y) 2))
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+; let x=1+1/x
+; we get x^2-x-1=0
+; x=\frac{1}{2}(1\pm\sqrt{5})
+; \phi=\frac{1}{2}(1+\sqrt{5}) is one of the fixed-points of x=>1+1/x
+
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+; 1.6180327868852458

ef2c1b75c27147c707b65c589dcfabf4eec62c2a
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-01-29 18:08:44 +0800

    Create sicp-ex-2-15.scm

---
+++ solutions/Chapter2/sicp-ex-2-15.scm
@@ -0,0 +1,5 @@
+#lang sicp
+
+; in the formula r1r2/(r1+r2), the r1(or r2) in numerator and denominator are exactly the same number
+; But our program cannot take them as the same
+; so par2 is better since every variable appears only once

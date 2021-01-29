824fb8f0b1ff177b2e86b98fc3d8ae6bc2682a06
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-01-29 21:32:10 +0800

    Create sicp-ex-2-26.scm

---
+++ solutions/Chapter2/sicp-ex-2-26.scm
@@ -0,0 +1,14 @@
+#lang sicp
+
+(define x (list 1 2 3))
+x
+; (1 2 3)
+(define y (list 4 5 6))
+y
+; (4 5 6)
+(append x y)
+; (1 2 3 4 5 6)
+(cons x y)
+; ((1 2 3) 4 5 6)
+(list x y)
+; ((1 2 3) (4 5 6))

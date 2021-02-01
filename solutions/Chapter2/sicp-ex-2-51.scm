b963173a52c566f7d1a4dac81d576fa7b66f2470
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-02-01 18:09:56 +0800

    Update sicp-ex-2-51.scm

--- solutions/Chapter2/sicp-ex-2-51.scm
+++ solutions/Chapter2/sicp-ex-2-51.scm
@@ -15,3 +15,9 @@
                      (make-vect 1.0 0.0)    ; new origin
                      (make-vect 0.0 0.0)    ; new end of edge1
                      (make-vect 1.0 1.0)))  ; new end of edge2
+
+(define (rotate180 painter)
+  (rotate90 (rotate90 paiter)))
+
+(define (rotate90 painter)
+  (rotate90 (rotate180 painter)))

c4c8fd2178f4652db82bd8d18879cbd2bc373490
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-02-08 22:08:36 +0800

    Create sicp-ex-2-81.scm

---
+++ solutions/Chapter2/sicp-ex-2-81.scm
@@ -0,0 +1,37 @@
+#lang sicp
+
+(define (scheme-number->scheme-number n) n)
+(define (complex->complex z) z)
+(put-coercion 'scheme-number 'scheme-number
+              scheme-number->scheme-number)
+(put-coercion 'complex 'complex complex->complex)
+
+(define (exp x y) (apply-generic 'exp x y))
+(put 'exp '(scheme-number scheme-number)
+     (lambda (x y) (tag (expt x y))))
+; a)
+; stuck in an infinite recursion
+; b) no. yes.
+; c)
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                  (cond ((= type1 type2) (error "No method for this type" a1))
+                        (t1->t2
+                         (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1
+                         (apply-generic op a1 (t2->t1 a2)))
+                        (else
+                         (error "No method for these types"
+                                (list op type-tags))))))
+              (error "No method for these types"
+                     (list op type-tags)))))))

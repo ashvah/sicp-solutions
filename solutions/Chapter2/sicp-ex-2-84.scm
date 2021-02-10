74434c3826fde3558132fbcd44e19e8e4a95506a
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-02-10 12:00:43 +0800

    Update sicp-ex-2-84.scm

--- solutions/Chapter2/sicp-ex-2-84.scm
+++ solutions/Chapter2/sicp-ex-2-84.scm
@@ -18,13 +18,15 @@
           current
           (iter (raise current) (inc n))))
     (iter arg (number-of-type (type-tag args))))
-  (let ((type-tags (map type-tag args)))
-    (let ((proc (get op type-tags)))
-      (if proc
-          (apply proc (map contents args))
-          (if (>= (length args) 2)
-              (let ((number-list (map number-of-type type-tags)))
-                (let ((n (max number-list)))
-                  (apply-generic op (map (lambda (arg) (raise-to n arg)) args))))
-              (error "No method for these types"
-                     (list op type-tags)))))))
+  (define (apply-generic-iter current-args)
+    (let ((type-tags (map type-tag current-args)))
+      (let ((proc (get op type-tags)))
+        (if proc
+            (apply proc (map contents current-args))
+            (if (>= (length args) 2)
+                (let ((number-list (map number-of-type type-tags)))
+                  (let ((n (max number-list)))
+                    (apply-generic-iter (map (lambda (arg) (raise-to n arg)) current-args))))
+                (error "No method for these types"
+                       (list op type-tags)))))))
+  (apply-generic-iter args))

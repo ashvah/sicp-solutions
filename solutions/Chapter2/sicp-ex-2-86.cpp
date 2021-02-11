0ac9f9f647f552741e17f4f8d13a2ea3fe71de01
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-02-11 21:02:12 +0800

    Create sicp-ex-2-86.cpp

---
+++ solutions/Chapter2/sicp-ex-2-86.cpp
@@ -0,0 +1,56 @@
+#lang sicp
+
+
+(put 'sine 'scheme-number
+     (lambda (x) (attach-tag 'scheme-number (make-scheme-number (sin x)))))
+(put 'cosine 'scheme-number 
+      (lambda (x) (attach-tag 'scheme-number (make-scheme-number (cos x)))))
+
+
+
+(put 'sine 'rational
+     (lambda (x) (attach-tag 'rational (make-rat (sin (/ (number x) (denom x))))))) 
+(put 'cosine 'rational
+     (lambda (x) (attach-tag 'rational (make-rat (cos (/ (number x) (denom x)))))))
+
+(define (sine x)
+  (apply-generic 'sine x))
+
+(define (cosine x)
+  (apply-generic 'cosine x))
+
+(define (install-complex-package)
+  ;; imported procedures from rectangular and polar packages
+  (define (make-from-real-imag x y)
+    ((get 'make-from-real-imag 'rectangular) x y))
+  (define (make-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'polar) r a))
+  ;; internal procedures
+  (define (add-complex z1 z2)
+    (make-from-real-imag (add (real-part z1) (real-part z2))
+                         (add (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (sub (real-part z1) (real-part z2))
+                         (sub (imag-part z1) (imag-part z2))))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (mul (magnitude z1) (magnitude z2))
+                       (add (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (div (magnitude z1) (magnitude z2))
+                       (sub (angle z1) (angle z2))))
+
+  ;; interface to rest of the system
+  (define (tag z) (attach-tag 'complex z))
+  (put 'add '(complex complex)
+       (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (lambda (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (lambda (z1 z2) (tag (div-complex z1 z2))))
+  (put 'make-from-real-imag 'complex
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'complex
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)

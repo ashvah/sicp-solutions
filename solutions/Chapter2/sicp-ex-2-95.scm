455d850e483da131eabd0916225a04e6976cd922
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-02-23 22:07:44 +0800

    Update sicp-ex-2-95.scm

--- solutions/Chapter2/sicp-ex-2-95.scm
+++ solutions/Chapter2/sicp-ex-2-95.scm
@@ -8,13 +8,3 @@
 ; (gcd-terms 908/169(x^2-2x+1) (remainder-terms Q2 908/169(x^2-2x+1)))
 ; (gcd-terms 908/169(x^2-2x+1) 0)
 ; 908/169(x^2-2x+1) .....
-
-(define (gcd-terms a b)
-  (if (empty-termlist? b)
-      a
-      (let ((f1 (first-term a))
-            (f2 (first-term b)))
-        (let ((o1 (order f1))
-              (o2 (order f2))
-              (c (coeff f2)))
-          (gcd-terms b (remainder-terms (mul a (make-term 0 (power c (+ 1 (- o1 o2))))) b))))))

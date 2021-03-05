b0803e4f947fd6273c8738830c159d91330a83c5
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-03-05 22:19:59 +0800

    Create sicp-ex-3-33.scm

---
+++ solutions/Chapter3/sicp-ex-3-33.scm
@@ -0,0 +1,32 @@
+#lang sicp
+
+(define (averager a b c)
+  (define (process-new-value)
+    (cond ((and (has-value? a) (has-value? b))
+           (set-value! c
+                       (/ (+ (get-value a) (get-value a)) 2.0)
+                       me))
+          ((and (has-value? a) (has-value? c))
+           (set-value! b
+                       (- (* 2 (get-value c)) (get-value a))
+                       me))
+          ((and (has-value? a) (has-value? c))
+           (set-value! a
+                       (- (* 2 (get-value c)) (get-value b))
+                       me))))
+  (define (process-forget-value)
+    (forget-value! c me)
+    (forget-value! a me)
+    (forget-value! b me)
+    (process-new-value))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value)  
+           (process-new-value))
+          ((eq? request 'I-lost-my-value) 
+           (process-forget-value))
+          (else 
+           (error "Unknown request -- ADDER" request))))
+  (connect a me)
+  (connect b me)
+  (connect c me)
+  me)

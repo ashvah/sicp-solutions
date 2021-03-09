56098c4653cace1a26903a3586306312b0433ac0
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-03-09 14:29:43 +0800

    Create sicp-ex-3-46.scm

---
+++ solutions/Chapter3/sicp-ex-3-46.scm
@@ -0,0 +1,8 @@
+#lang sicp
+
+;                (false)
+;    (car cell)
+;                           (car cell)
+; (set-car! cell true)
+;                        (set-car! cell true)
+;                (true)

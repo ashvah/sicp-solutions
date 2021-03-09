3426013b571dadc3907a953a532a36187b40f13c
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-03-09 14:16:02 +0800

    Create sicp-ex-3-44.scm

---
+++ solutions/Chapter3/sicp-ex-3-44.scm
@@ -0,0 +1,9 @@
+#lang sicp
+
+; No need to do this
+; In exchange, we need to access the balance of both account
+; And we should make sure the balance won't change before the exchange finish
+; So we need the serialize the whole process
+
+; But from-count is ok
+; we never change the sum of balance of all accounts

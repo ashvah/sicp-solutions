1f8fbdd06c48cf926f6ad00c8c280e1d8f10ad35
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-03-12 22:52:51 +0800

    Create sicp-ex-3-57.scm

---
+++ sicp-ex-3-57.scm
@@ -0,0 +1,20 @@
+#lang sicp
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define fibs
+  (cons-stream 0
+               (cons-stream 1
+                            (add-streams (stream-cdr fibs)
+                                         fibs))))
+
+(stream-ref fibs n)
+; (0 (1 (add-streams (stream-cdr fibs) fibs)))
+; (1 (add-streams (stream-cdr fibs) fibs))
+; (add-streams (stream-cdr fibs) fibs)
+; (stream-map + (stream-cdr fibs) fibs)
+; (0+1 (stream-map ...))  1   1
+; (1+1 (stream-map ...))  2   1+1+1
+;
+; (fib(n) (stream-map ...))  n-1  T(n-1)+1+T(n-1)=O(2^n)

8f6201e9fc8bdbbf87456a382671431acaeb2bef
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-03-14 16:25:46 +0800

    Create sicp-ex-3-68.scm

---
+++ solutions/Chapter3/sicp-ex-3-68.scm
@@ -0,0 +1,19 @@
+#lang sicp
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t)
+  (interleave
+   (stream-map (lambda (x) (list (stream-car s) x))
+               t)
+   (pairs (stream-cdr s) (stream-cdr t))))
+
+(define p (pairs integers integers))
+
+(display-stream-n p 15)
+
+; infinite loop

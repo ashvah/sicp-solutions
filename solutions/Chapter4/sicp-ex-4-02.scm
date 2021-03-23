ba03d1ddbfba769fb7bd59d80a1622ce3d32e3b7
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-03-23 19:46:26 +0800

    Create sicp-ex-4-02.scm

---
+++ solutions/Chapter4/sicp-ex-4-02.scm
@@ -0,0 +1,36 @@
+#lang sicp
+
+; a)
+
+(define x 3)
+(apply (eval 'define env) (list-of-values '(x 3) env))
+
+; b)
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env))
+        ((begin? exp) 
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        (else
+         (error "Unknown expression type -- EVAL" exp))))
+
+
+
+(define (application? exp)
+  (tagged-list? exp 'call))
+
+(define (operator exp) (cadr exp))
+(define (operands exp) (cddr exp))

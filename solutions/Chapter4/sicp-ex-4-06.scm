b928da7c16e54d481c0e399e6625fe6fb279d57b
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-03-24 12:11:55 +0800

    Create sicp-ex-4-06.scm

---
+++ solutions/Chapter4/sicp-ex-4-06.scm
@@ -0,0 +1,24 @@
+#lang sicp
+
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+(define (let-vars exp)
+  (map car (cadr exp)))
+
+(define (let-exps exp)
+  (map cadr (cadr exp)))
+
+(define (let-body exp)
+  (cddr exp))
+
+(define (let-combination exp env)
+  (cons 'call (cons (make-lambda (let-vars exp) (let-body exp)) (let-exps exp))))
+
+(define eval-let
+  (lambda (exp env) (eval (let-combination exp) env)))
+
+(put 'eval 'let let-combination)
+
+(let-combination '(let ((x 1) (y 2)) (+ x y)) '())
+; (call (lambda (x y) (+ x y)) 1 2)

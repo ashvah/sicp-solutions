2e93eddc5933069610db8f380e72cd039c26462a
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-03-24 12:27:53 +0800

    Update A_Data_Directed_Style_McEval.scm

--- Examples/A_Data_Directed_Style_McEval.scm
+++ Examples/A_Data_Directed_Style_McEval.scm
@@ -38,85 +38,32 @@
 (define put (operation-table 'insert-proc!))
 
 ;---------------------------------------------------------
-; eval
+; core
 
+; call
 (define (list-of-values exps env)
   (if (no-operands? exps)
       '()
       (cons (eval (first-operand exps) env)
             (list-of-values (rest-operands exps) env))))
 
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+
+(define (no-operands? ops) (null? ops))
+(define (first-operand ops) (car ops))
+(define (rest-operands ops) (cdr ops))
+
+(define eval-call
+  (lambda (exp env)
+    (apply (eval (operator exp) env)
+           (list-of-values (operands exp) env))))
+
+; if
 (define (eval-if exp env)
   (if (true? (eval (if-predicate exp) env))
       (eval (if-consequent exp) env)
       (eval (if-alternative exp) env)))
-
-(define (eval-sequence exps env)
-  (cond ((last-exp? exps) (eval (first-exp exps) env))
-        (else (eval (first-exp exps) env)
-              (eval-sequence (rest-exps exps) env))))
-
-(define (eval-assignment exp env)
-  (set-variable-value! (assignment-variable exp)
-                       (eval (assignment-value exp) env)
-                       env)
-  'ok)
-
-(define (eval-definition exp env)
-  (define-variable! (definition-variable exp)
-                    (eval (definition-value exp) env)
-                    env)
-  'ok)
-
-(define (self-evaluating? exp)
-  (cond ((number? exp) true)
-        ((string? exp) true)
-        (else false)))
-
-(define (quoted? exp)
-  (tagged-list? exp 'quote))
-
-(define (text-of-quotation exp) (cadr exp))
-
-(define (tagged-list? exp tag)
-  (if (pair? exp)
-      (eq? (car exp) tag)
-      false))
-
-(define (variable? exp) (symbol? exp))
-
-(define (assignment? exp)
-  (tagged-list? exp 'set!))
-
-(define (assignment-variable exp) (cadr exp))
-
-(define (assignment-value exp) (caddr exp))
-
-
-(define (definition? exp)
-  (tagged-list? exp 'define))
-
-(define (definition-variable exp)
-  (if (symbol? (cadr exp))
-      (cadr exp)
-      (caadr exp)))
-
-(define (definition-value exp)
-  (if (symbol? (cadr exp))
-      (caddr exp)
-      (make-lambda (cdadr exp)
-                   (cddr exp))))
-
-(define (lambda? exp) (tagged-list? exp 'lambda))
-
-(define (lambda-parameters exp) (cadr exp))
-(define (lambda-body exp) (cddr exp))
-
-(define (make-lambda parameters body)
-  (cons 'lambda (cons parameters body)))
-
-
-(define (if? exp) (tagged-list? exp 'if))
 
 (define (if-predicate exp) (cadr exp))
 
@@ -130,8 +77,11 @@
 (define (make-if predicate consequent alternative)
   (list 'if predicate consequent alternative))
 
-
-(define (begin? exp) (tagged-list? exp 'begin))
+; begin
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps) (eval (first-exp exps) env))
+        (else (eval (first-exp exps) env)
+              (eval-sequence (rest-exps exps) env))))
 
 (define (begin-actions exp) (cdr exp))
 
@@ -146,18 +96,74 @@
 
 (define (make-begin seq) (cons 'begin seq))
 
+(define eval-begin
+  (lambda (exp env)
+    (eval-sequence (begin-actions exp) env)))
 
-(define (application? exp) (pair? exp))
-(define (operator exp) (car exp))
-(define (operands exp) (cdr exp))
+; set!
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+                       (eval (assignment-value exp) env)
+                       env)
+  'ok)
 
-(define (no-operands? ops) (null? ops))
-(define (first-operand ops) (car ops))
-(define (rest-operands ops) (cdr ops))
+(define (assignment-variable exp) (cadr exp))
 
+(define (assignment-value exp) (caddr exp))
 
-(define (cond? exp) (tagged-list? exp 'cond))
+; define
+(define (eval-definition exp env)
+  (define-variable! (definition-variable exp)
+                    (eval (definition-value exp) env)
+                    env)
+  'ok)
 
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caadr exp)))
+
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdadr exp)
+                   (cddr exp))))
+
+; self-evaluating
+(define (self-evaluating? exp)
+  (cond ((number? exp) true)
+        ((string? exp) true)
+        (else false)))
+
+; quote
+(define (text-of-quotation exp) (cadr exp))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+(define eval-quote
+  (lambda (exp env)
+    (text-of-quotation exp)))
+
+; variable
+(define (variable? exp) (symbol? exp))
+
+; lambda
+(define (lambda-parameters exp) (cadr exp))
+(define (lambda-body exp) (cddr exp))
+
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+(define eval-lambda
+  (lambda (exp env)
+    (make-procedure (lambda-parameters exp)
+                    (lambda-body exp)
+                    env)))
+
+;cond
 (define (cond-clauses exp) (cdr exp))
 
 (define (cond-else-clause? clause)
@@ -197,29 +203,12 @@
                          (sequence->exp (cond-actions first))
                          (expand-clauses rest)))))))
 
-(define eval-quote
-  (lambda (exp env)
-    (text-of-quotation exp)))
-
-(define eval-lambda
-  (lambda (exp env)
-    (make-procedure (lambda-parameters exp)
-                    (lambda-body exp)
-                    env)))
-
-(define eval-begin
-  (lambda (exp env)
-    (eval-sequence (begin-actions exp) env)))
-
 (define eval-cond
   (lambda (exp env)
     (eval (cond->if exp) env)))
 
-(define eval-call
-  (lambda (exp env)
-    (apply (eval (operator exp) env)
-           (list-of-values (operands exp) env))))
 
+; and-or
 (define (get-clauses exp) (cdr exp))
 
 (define (first-clause clauses) (car clauses))
@@ -250,6 +239,23 @@
   (lambda (exp env)
     (eval-or-clauses (get-clauses exp) env)))
 
+; let
+(define (let-vars exp)
+  (map car (cadr exp)))
+
+(define (let-exps exp)
+  (map cadr (cadr exp)))
+
+(define (let-body exp)
+  (cddr exp))
+
+(define (let-combination exp env)
+  (cons 'call (cons (make-lambda (let-vars exp) (let-body exp)) (let-exps exp))))
+
+(define eval-let
+  (lambda (exp env) (eval (let-combination exp) env)))
+
+; install
 (put 'eval 'quote eval-quote)
 (put 'eval 'set! eval-assignment)
 (put 'eval 'define eval-definition)
@@ -260,11 +266,14 @@
 (put 'eval 'call eval-call)
 (put 'eval 'and eval-and)
 (put 'eval 'or eval-or)
+(put 'eval 'let let-combination)
 
+; eval
 (define (eval exp env)
   (cond ((self-evaluating? exp) exp)
         ((variable? exp) (lookup-variable-value exp env))
-        (else (let ((proc (get 'eval (operator exp))))
+        ((not (pair? exp)) (error "Unknown expression type -- EVAL" exp))
+        (else (let ((proc (get 'eval (car exp))))
                 (if proc
                     (proc exp env)
                     (error "Unknown expression type -- EVAL" exp))))))

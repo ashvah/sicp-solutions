444f2604a0d70f5617fe61f975f2ad370ccfd402
Soaring <74124053+SSSoaring@users.noreply.github.com>
2021-05-09 14:34:30 +0800

    Create sicp-ex-4-30.scm

---
+++ solutions/Chapter4/sicp-ex-4-30.scm
@@ -0,0 +1,14 @@
+#lang sicp
+
+; a)
+; display will force-it
+
+; b)
+; original: (1 2), 1
+; Cy's method: (1 2), (1 2)
+
+; c)
+; if (thunk? obj) is false, it'll return obj directly
+
+; d)
+; Cy's
